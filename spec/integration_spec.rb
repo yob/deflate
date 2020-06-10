19bfcda762083bf6df036838af9fbffaf1fa4eb5
James Healy <james@yob.id.au>
2020-06-10 23:49:28 +1000

    add failing spec for file that uses a dynamic huffman table

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,13 +3,32 @@ require 'rspec'
 RSpec.describe "Integration" do
   let(:bitio) { Deflate::BitIO.new(input)}
 
+  # hello-world.z is:
+  #
+  # * zlib container format
+  # * a single deflate block, encoding type 1 (static huffman)
   context "with hello-world.z" do
     let(:path) { File.join(File.expand_path(File.dirname(__FILE__)), "fixtures", "hello-world.z") }
 
     it "can decompress the file" do
       File.open(path, "rb") do |io|
         file = Deflate::File.new(io)
-        expect(file.out).to eql("hello world\n")  
+        expect(file.out).to eql("hello world\n")
+      end
+    end
+  end
+
+  # lorem-ipsum.z is:
+  #
+  # * zlib container format
+  # * a single deflate block, encoding type 2 (dynamic huffman)
+  context "with hello-world.z" do
+    let(:path) { File.join(File.expand_path(File.dirname(__FILE__)), "fixtures", "lorem-ipsum.z") }
+
+    it "can decompress the file" do
+      File.open(path, "rb") do |io|
+        file = Deflate::File.new(io)
+        expect(file.out).to eql("hello world\n")
       end
     end
   end
